bd14b3bb53e3dc5898ced42cefa0f981e1554801
Gaston Maron <gastonmaron@gmail.com>
2019-12-19 20:03:17 -0300

    Change function Total variable is not passed by parameter. The action return the unit operation.

--- operations/operations.py
+++ operations/operations.py
@@ -1,56 +1,44 @@
-from functools import partial
-import pytest
-
 
 class Operations:
 
     @staticmethod
-    def add(a: int, b: int):
-        return a + b
+    def add(a: int):
+        return +a
 
     @staticmethod
-    def sub(a: int, b: int):
-        return a - b
-
-    @staticmethod
-    def div(a: int, b: int):
-        assert b > 0, "Zero division error!"
-        return a / b
-
-    @staticmethod
-    def mul(a: int, b: int):
-        return a * b
+    def sub(a: int):
+        return -a
 
     @staticmethod
     def power(base: int, exp: int):
         assert exp != 0, "Zero exponent error!"
+        if exp < 0:
+            assert base != 0, "Zero division error!"
         return base ** exp
 
     @staticmethod
-    def dob(a: int, b: int):
-        return a + (b * 2)
+    def dob(a: int):
+        return a * 2
 
     @staticmethod
-    def squared(a: int, b: int):
-        return a + b ** 2
+    def squared(a: int):
+        return a ** 2
 
     @staticmethod
-    def cube(a: int, b: int):
-        cube_operand = partial(Operations.power, exp=3)
-        return a + cube_operand(b)
+    def cube(a: int):
+        return Operations.power(base=a, exp=3)
 
     @staticmethod
-    def inverse_multiplicative(a: int, b: int):
-        assert b > 0, "Zero division error!"
+    def inverse_multiplicative(a: int):
+        assert a > 0, "Zero division error!"
         #  Another way is to exponent the operator to -1
         #  x ^ -1 = 1 / x
         #  minus_one_operator = partial(Operations.power, exp=-1)
-        return a + Operations.div(1, b)
-
+        return 1 / a
 
     @staticmethod
-    def martin_chaia_function(arr: [], action):
+    def martin_chaia_function(arr: list, action):
         total: int = 0
         for i in arr:
-            total = action(total, i)
+            total = total + action(i)
         return total
